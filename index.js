7a52c827950a8f4a47bbeda888c545598f07e567
Wildan Hidayat <mwildanhidayat5@gmail.com>
2025-08-14 01:58:23 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="0;url=https://contoh.com">
+    <title>Redirecting LinkedIn...</title>
+    <script>
+        window.location.href = "https://www.linkedin.com/in/m-wildan-hidayat-905780330?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app";
+    </script>
+</head>
+<body></body>
+</html>
